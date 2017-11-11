9937e4492dde4881f461a7e281123136299e69c0
David G <david.a.grs@gmail.com>
2017-11-11 19:51:37 +0100

    implicit sharing bench

--- course01/implicit_sharing.cc
+++ course01/implicit_sharing.cc
@@ -1,5 +1,27 @@
 #include <QString>
 
+#include <chrono>
+#include <string>
+#include <iostream>
+
+template <typename Callable>
+void run_benchmark(const std::string& desc, std::size_t iterations, Callable&& callable)
+{
+    auto start = std::chrono::steady_clock::now();
+
+    for (std::size_t i = 0; i < iterations; ++i)
+        callable();
+
+    auto end = std::chrono::steady_clock::now();
+
+    double total_time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << desc << ": total_time=" << total_time << "ms";
+
+    double per_iteration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / double(iterations);
+    std::cout << " per_iteration=" << per_iteration << "ns" << std::endl;
+}
+
+
 QString __attribute__ ((noinline)) CopyString(const QString& str)
 {
   return str;
@@ -8,9 +30,15 @@ QString __attribute__ ((noinline)) CopyString(const QString& str)
 int main()
 {
   QString str = "foo";
-  QString copy = CopyString(str);
+  int sum = 0;
+
+  run_benchmark("implicit_sharing", 10e6, [&]()
+  {
+    QString copy = CopyString(str);
+    sum += copy.size();
+  });
 
   str += "bar";
-  return copy.size();
+  return 0;
 }
 
